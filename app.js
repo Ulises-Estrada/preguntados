82e70018fcd1adeddd8423fb0d3fef7e86468c9a
Ulises-Estrada <ulisesestradaceja@gmail.com>
2026-08-13 14:52:56 -0600

    Implement game logic and update categories in the quiz

--- app.js
+++ app.js
@@ -12,9 +12,56 @@ const squares = document.querySelectorAll(".standard");
 const headerTitle = document.querySelector(".header-title");
 
 // Game Values
-let isGameActive = false;
+let isGameActive = localStorage.getItem("isGameActive") || false;
 let isPlayerActive = false;
 let errors = 0;
 let points = 0;
 let rounds = 0;
 let roundsLimit = 25;
+let currentLength = -4.86;
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min - 1)) + 1;
+}
+
+function activeGame() {
+  headerTitle.addEventListener("click", () => {
+    setGameValuesLS();
+    isPlayerActive = true;
+    marker.computedStyleMap.pointerEvents = "auto";
+    headerTitle.style.pointerEvents = "none";
+    headerTitle.innerText = `Ronda 0 / ${roundsLimit}`;
+  });
+}
+
+function setGameValuesLS() {
+  localStorage.setItem("points", 0);
+  localStorage.setItem("errors", 0);
+  localStorage.setItem("rounds", 0);
+  localStorage.setItem("isGameActive", true);
+}
+
+activeGame();
+
+// Marker event listener
+
+marker.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (!isPlayerActive) return false;
+  marker.style.pointerEvents = "none";
+
+  localStorage.getItem("rounds");
+  rounds++;
+  headerTitle.innerText = `Ronda ${rounds} / ${roundsLimit}`;
+  currentLength += 900 + getRandomInt(0, 37) * 9.729;
+  rouletteContainer.style.transform = "rotate(-" + currentLength + "deg)";
+
+  const gameTime = setTimeout(async () => {
+    const filtered = Array.from(squares).sort(
+      (a, b) =>
+        a.getBoundingClientRect().right - b.getBoundingClientRect().right,
+    );
+    const categorySelected = filtered[0].dataset.category;
+  }, 5000);
+});
